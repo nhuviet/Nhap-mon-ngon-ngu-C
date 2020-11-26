73947831a83a136609525736241811eeb33233e1
nhuviet <72543029+nhuviet@users.noreply.github.com>
2020-11-26 16:46:42 +0700

    Update chuvi.c

--- Bài tập 1/chuvi.c
+++ Bài tập 1/chuvi.c
@@ -2,14 +2,15 @@
 
 int main(){
 	//Khai bao bien R,PI,CV,DT kieu double
-	double R ,PI ,CV ,DT;
+	double R,CV ,DT;
+	//khai bao hang pi
+	const float PI=3.14;
 	//gan gia tri bien
 	R = 2.5;
-	PI = 3.14;
 	CV = 2 * R * PI;
-	DT = R * R * PI;
+	DT = R * R *PI;
 	//in ra man hinh
-	printf("Chu vi \t Dien tich \n %0.1f \t %0.1f",CV,DT);
+	printf("Chu vi \t Dien tich \n%0.1f \t %0.2lf",CV,DT);
 	return 0;
 	
 }

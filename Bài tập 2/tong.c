1e3b0d61b74cb3296321dab992eaec168a556753
nhuviet <72543029+nhuviet@users.noreply.github.com>
2020-12-03 13:56:13 +0700

    Create tong.c

---
+++ Bài tập 2/tong.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	//khai bao bien a,b
+	int a,b;
+	//Nhap gia tri a,b tu ban phim
+	printf("Nhap so a: ");scanf("%d",&a);
+	printf("Nhap so b: ");scanf("%d",&b);
+	//in gia tri ra man hinh
+	printf("a+b=%d",a+b);
+	return 0;
+}

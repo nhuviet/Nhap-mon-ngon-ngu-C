e8bb100213182c29488e8dd0a97347104b64c1c2
nhuviet <72543029+nhuviet@users.noreply.github.com>
2020-12-03 14:51:41 +0700

    Update chieudai.c

--- Bài tập 2/chieudai.c
+++ Bài tập 2/chieudai.c
@@ -7,6 +7,7 @@ int main(){
 	//Nhap gia tri tu ban phim
 	printf("Nhap chieu dai(m): ");scanf("%d",&l);
 	//in gia tri ra ma hinh
-	printf("Met(m)\t|Xentimet(cm)\t|Milimet(mm)\n-------------------------------------\n%d\t|%d\t\t|%d",l,l*100,l*1000);
+	printf("____________________________________________________\n");
+	printf("Met(m)\t|\tXentimet(cm)\t|\tMilimet(mm)\n-----------------------------------------------\n%d\t|\t%d\t\t|\t%d\t",l,l*100,l*1000);
 	return 0;
 }

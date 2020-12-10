bd79a00814d577169949d9a4b4e51cd8e58dfec9
nhuviet <72543029+nhuviet@users.noreply.github.com>
2020-12-10 15:31:22 +0700

    Update sosanh.c

--- Bài tập 3/sosanh.c
+++ Bài tập 3/sosanh.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	float a,b;
+	float a,b;//khai bao bien a,b
+	//nhap so a va b tu ban phim
 	printf("Nhap so a: ");scanf("%f",&a);
 	printf("Nhap so b: ");scanf("%f",&b);	
+	//so sanh a va b
 	if (a>b){
 		printf("a>b");
 	}

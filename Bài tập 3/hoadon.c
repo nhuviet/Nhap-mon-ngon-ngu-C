b6e769cd6e43662ec764ac76e77af341f54710f7
nhuviet <72543029+nhuviet@users.noreply.github.com>
2020-12-10 16:40:10 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -5,19 +5,19 @@ int main(){
 	float dongia,thanhtien;
 	int soluong;
 	//Nhap gia cua mon hang va so luong
-	printf("Nhap so luong muon mua: ");scanf("%d",&soluong);
-	printf("Nhap gia mon hang: ");scanf("%f",&dongia);
+	printf("Nhap so luong muon mua: ");
+	scanf("%d",&soluong);
+	printf("Nhap gia mon hang: ");
+	scanf("%f",&dongia);
 	
 	if (soluong>=5){//kiem tra so luong mua >=5
 		//giam gia 25% = 75% gia tri don hang
 		thanhtien=(dongia*soluong)*75/100;
-		printf("Don gia\t\tSo luong\t\Thanh tien\n");
-		printf("%g\t\t%d\t\t%g",dongia,soluong,thanhtien);
+		printf("Thanh tien: %g",thanhtien);
 	}
 	else{//so luong mua duoi 5
 		thanhtien=dongia*soluong;
-		printf("Don gia\t\tSo luong\t\Thanh tien\n");
-		printf("%g\t\t%d\t\t%g",dongia,soluong,thanhtien);
+		printf("Thanh tien: %g",thanhtien);
 	}
 	return 0;
 }

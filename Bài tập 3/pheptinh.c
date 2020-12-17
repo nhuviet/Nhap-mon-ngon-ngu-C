6753a35aeca4c17fe4eff570ec3739f1126899ac
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-17 15:50:23 +0700

    Create pheptinh.c

---
+++ Bài tập 3/pheptinh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+//Khai bao bien a,b,pt
+	int a=5,b=2;
+	char pt;
+  
+	printf("Vui long nhap phep tinh: ");
+	scanf("%c",&pt);
+  
+	switch (pt){
+		case '+': //Nhap +
+			printf("a + b = %d", a+b);
+			break;
+		case '-': //Nhap -
+			printf("a - b = %d", a-b);
+			break;
+		default: // truong hop con lai
+			printf("Vui long nhap phep tinh: ");
+			break;
+	}
+	return 0;
+}

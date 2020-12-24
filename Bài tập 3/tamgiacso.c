ecea13c26b40cd0d6ea50d3ec88a718c40ee6f32
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-24 17:00:09 +0700

    Create tamgiacso.c

---
+++ Bài tập 3/tamgiacso.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+	int c, n, sum=0;
+	
+	printf("n = ");scanf("%d",&n);
+	
+	for (c=1;c<=n;c++){
+		printf("%d ",c);
+		for (c = 1; c<=n; c++){
+			
+		}
+	}
+
+/*	switch (n){
+		case 1:
+			printf("S = %d ",sum);
+			break;
+		case 2:
+			printf("S = 1 + %d = %d ",n,sum);
+			break;
+		case 3:
+			printf("S = 1 + 2 + %d = %d ",n,sum);
+			break;
+		default:
+			printf("S = 1 + 2 +...+ %d = %d ",n,sum);
+	}*/
+	
+	return 0;
+}

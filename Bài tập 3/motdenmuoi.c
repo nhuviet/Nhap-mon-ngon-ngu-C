25f8016e6160257c568463650a7bb64d548d8237
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-24 13:44:35 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int x;
+	printf("so 1 den 10 tren 1 hang:\n");
+	for (x=1;x<=10;x++){
+		printf("%d ",x);
+	}
+	printf("\nso 1 den 10 tren 10 hang:\n");
+	for (x=1;x<=10;x++){
+		printf("%d\n",x);
+	}
+	return 0;
+}

0ae033e0065b1bfcddd50a5577648f01be6b686f
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-24 16:24:13 +0700

    Create tongn.c

---
+++ Bài tập 3/tongn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	int c, n, sum = 0;
+  
+	printf("Nhap so n:");
+  scanf("%d",&n);
+  
+	printf("n = %d\n",n);
+  
+	for (c=1;c<=n;c++){
+		sum += c;
+	}
+  
+	// In Ket qua
+	switch (n){
+		case 1:
+			printf("S = %d ",sum);
+			break;
+		case 2:
+			printf("S = 1 + %d = %d ",n,sum);
+			break;
+		case 3:
+			printf("S = 1 + 2 + %d = %d ",n,sum);
+			break;
+		default:
+			printf("S = 1 + 2 +...+ %d = %d ",n,sum);
+	}
+	
+	return 0;
+}

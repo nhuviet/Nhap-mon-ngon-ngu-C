eb880858ab533fef223a887b5369b0beb7da1a14
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-25 08:54:18 +0700

    Update tamgiacso.c

--- Bài tập 3/tamgiacso.c
+++ Bài tập 3/tamgiacso.c
@@ -5,7 +5,7 @@ int main(){
 	
 	printf("Nhap n=");scanf("%d",&n);//nhap n
 	
-	for (i=1 ;i<=n; i++){//vong lap in cot
+	for (i=1 ;i<=n; i++){//vong lap in hang
 		printf("\n");
 		for (j=1; j<=i; j++){//vong lap in so
 			printf("%d ",j);

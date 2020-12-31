e87f32c554401d69063319197ea245f4544fa1f0
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2020-12-31 21:09:35 +0700

    Create kt_05.c

---
+++ Bài tập 3/kt_05.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n, i, sum_c=0, sum_l=0;
+
+    printf("Nhap n: ");scanf("%d",&n);
+    for (i=1;i<=n;i++){
+    	if (i%2==0){ //so chan
+    		sum_c+=i;
+		}
+		else{ //so le
+			sum_l+=i;
+		}
+	}
+    printf("Tong cac so chan = %d\n",sum_c);
+    printf("Tong cac so le = %d",sum_l);
+    return 0;
+}

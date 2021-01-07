98e52b48d7ac5d71cafc17097a9391a01723742e
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-07 16:13:02 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,13 +2,29 @@
 
 int main(){
 	int a,b;
-  
-	printf("Bang cuu chuong\n");
-  
-	for (a=1;a<=9;a++){
-		printf("Bang cuu chuong %d\n",a);
-		for (b=1;b<=9;b++){
-			printf("%d * %d = %d\n",a,b,a*b);
-		}
-	}
+	printf("Bang 2\n");
+	a = 2;b = 1;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 2;b = 2;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 2;b = 3;
+	printf("%d x %d = %d\n",a,b,a*b);
+	
+	printf("Bang 3\n");
+	a = 3;b = 1;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 3;b = 2;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 3;b = 3;
+	printf("%d x %d = %d\n",a,b,a*b);
+	
+	printf("Bang 4\n");
+	a = 4;b = 1;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 4;b = 2;
+	printf("%d x %d = %d\n",a,b,a*b);
+	a = 4;b = 3;
+	printf("%d x %d = %d\n",a,b,a*b);
+	
+	return 0;
 }

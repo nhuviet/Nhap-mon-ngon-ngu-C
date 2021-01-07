6be20172c9e01a5a7089c9e5f97707ff07864b18
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-07 16:39:29 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -3,27 +3,30 @@
 int main(){
 	int a,b;
 	printf("Bang 2\n");
-	a = 2;b = 1;
+	a = 2;
+	b = 1;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 2;b = 2;
+	b = 2;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 2;b = 3;
+	b = 3;
 	printf("%d x %d = %d\n",a,b,a*b);
 	
 	printf("Bang 3\n");
-	a = 3;b = 1;
+	a = 3;
+	b = 1;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 3;b = 2;
+	b = 2;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 3;b = 3;
+	b = 3;
 	printf("%d x %d = %d\n",a,b,a*b);
 	
 	printf("Bang 4\n");
-	a = 4;b = 1;
+	a = 4;
+	b = 1;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 4;b = 2;
+	b = 2;
 	printf("%d x %d = %d\n",a,b,a*b);
-	a = 4;b = 3;
+	b = 3;
 	printf("%d x %d = %d\n",a,b,a*b);
 	
 	return 0;

adb8a26472e9a2fca790e374c4dee3b2f0f007fb
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-08 08:03:19 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -1,13 +1,13 @@
 int test03(){
-	int n,i,sum=0;
+	int n,so_du,sum=0;
 	printf("Nhap so n(2 chu so): "); scanf("%d",&n);
     
-	i = n%10; 
-	sum+= i;
+	so_du = n%10; 
+	sum+= so_du;
 	n/= 10;
 	
-	i = n%10;
-	sum+= i;
+	so_du = n%10; 
+	sum+= so_du;
 	n/= 10;
 		
 	printf("%d\n",sum);

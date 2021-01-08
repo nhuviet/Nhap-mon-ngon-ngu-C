e9adf0166b0f5607af4a34720beb4d92f819dcbe
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-08 22:11:50 +0700

    Update kt_06_b.c

--- Bài tập 3/kt_06_b.c
+++ Bài tập 3/kt_06_b.c
@@ -6,14 +6,10 @@ int main()
 	printf("\nNhap chieu cao cua hinh tam giac : ");
 	scanf("%d", &n);
 
-	for (i=1; i<=n; i++) 
-	{
+	for (i=1; i<=n; i++){ 
 
  		for (j=1; j<=(2*n-1);j++){
-			if (j==(n-(i-1)) || j==(n+(i-1)) ){
-				printf(" * ");
-			}
-			else if ( i == n){
+			if (j==(n-(i-1)) || j==(n+(i-1)) || i == n){ //canh bên trai: j==(n-(i-1)), canh ben phai: j==(n+(i-1), i==n la canh day
 				printf(" * ");
 			}
 			else{
@@ -23,6 +19,5 @@ int main()
 
 		printf("\n");
 	}
-	getch();
+	return 0;
 }
-

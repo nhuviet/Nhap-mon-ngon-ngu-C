2be2eb374365272e68d10c01c7a13bf376900dc5
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-08 09:41:46 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -1,9 +1,24 @@
+DOR LOOP
 int main(){
 	int n,so_du,sum=0;
 	printf("Nhap so n (so nguyen <1000): "); scanf("%d",&n);
     
 	for(;n!=0;n=n/10){
 		sum += n%10;
+	}
+	
+	printf("sum = %d\n",sum);
+	return 0;
+}
+
+WHILE LOOP
+int main(){
+	int n,so_du,sum=0;
+	printf("Nhap so n (so nguyen <1000): "); scanf("%d",&n);
+    
+	while (n!=0){
+        	sum += n % 10;
+		n/= 10;
 	}
 	
 	printf("sum = %d\n",sum);

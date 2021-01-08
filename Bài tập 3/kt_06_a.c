b490caae316d15a0fe3a8ec3e23c8892cd62ea25
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-08 15:05:05 +0700

    Update kt_06_a.c

--- Bài tập 3/kt_06_a.c
+++ Bài tập 3/kt_06_a.c
@@ -5,12 +5,12 @@ int main(){
 
 	printf("\nNhap n: ");scanf("%d", &n);
 
-	for (i=0; i<n; i++){
- 		for (space=n-1; space>i; space--){
-			printf("   ");
+	for (i=1; i<=n; i++){
+ 		for (space=n-1; space>=i; space--){
+			printf(" ");
 		}
-		for (c=0; c<i*2+1; c++){
- 			printf(" * ");
+		for (c=1; c<=i*2-1; c++){
+ 			printf("*");
  		}
 	printf("\n");
   	}

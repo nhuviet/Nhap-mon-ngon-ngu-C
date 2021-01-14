e2b46c949ba9e94f2a48a0aaf32c0c17845565cf
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-14 15:21:07 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	int a[4]={3,5,7,2};
+	int a[4]={3,5,7,2};//mang a[2][2]={{1,2},{3,4}}
 	int i,sum=0;
 	for (i=0;i<4;i++){
 		printf("Phan tu thu %d: %d\n",i+1,a[i]);
-		sum+=a[i];
+		sum+=a[i];//cap nhat gia tri sum voi cac phan tu trong mang
 	}
 	printf("Tong cac phan tu trong mang: %d",sum);
 	return 0;	

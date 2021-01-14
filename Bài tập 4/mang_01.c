7271ff5ce5ef2567751da1a662fc3f71f1448c0e
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-14 13:54:37 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -2,9 +2,11 @@
 
 int main(){
 	int a[4]={3,5,7,2};
-	int i;
+	int i,sum=0;
 	for (i=0;i<4;i++){
 		printf("Phan tu thu %d: %d\n",i+1,a[i]);
+		sum+=a[i];
 	}
+	printf("Tong cac phan tu trong mang: %d",sum);
 	return 0;	
 }

cc46cbd807bcaad6a9e33f9c51a4579dec62f413
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-01-22 09:10:16 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -15,3 +15,25 @@ int main(){
 	printf("%d",Sum(a,b,c));
 	return 0;
 }
+
+
+//khong gia tri tra ve 
+
+#include <stdio.h>
+void Sum(int a,int b,int c);
+
+void Sum(int a, int b, int c){
+	printf("%d",a+b+c);
+	
+}
+
+int main(){
+	int a,b,c;
+	
+	printf("Nhap so a: ");scanf("%d",&a);
+	printf("Nhap so b: ");scanf("%d",&b);
+	printf("Nhap so c: ");scanf("%d",&c);
+
+	Sum(a,b,c);
+	return 0;
+}

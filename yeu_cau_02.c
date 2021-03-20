bf5de5fee9cd84f19e4a00a734cbf1d10f7b7caf
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-03-20 14:27:45 +0700

    Create yeu_cau_02.c

---
+++ yeu_cau_02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+float chu_vi(float r);
+float dien_tich(float r);
+float const pi=3.14;
+
+void main(){
+	float r;
+	printf("Tinh chu vi, dien tich hinh tron\n");
+	printf("Nhap ban kinh hinh tron r = ");scanf("%f",&r);
+	printf("Chu vi hinh tron ban kinh %.2f C = %.2f\n",r,chu_vi(r));
+	printf("Dien tich hinh tron ban kinh %.2f S = %.2f",r,dien_tich(r));
+}
+
+float chu_vi(float r){
+	float result;
+	result=2*pi*r;
+	return result;
+}
+
+float dien_tich(float r){
+	float result;
+	result=pi*pow(r,2);
+	return result;
+}

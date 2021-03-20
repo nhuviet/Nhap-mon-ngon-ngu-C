70a7bea304f23426947d4552a5c24c895b5986e7
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-03-20 14:55:34 +0700

    Update Yeu_cau_03.c

--- Yeu_cau_03.c
+++ Yeu_cau_03.c
@@ -3,7 +3,7 @@ int lcm(int a, int b);
 
 int main(){
 	int a,b;
-	printf("Tim boi chung nho nhat cua 2 so(<10000)\n");
+	printf("Tim boi chung nho nhat cua 2 so(<100000)\n");
 	printf("Nhap so a: ");scanf("%d",&a);
 	printf("Nhap so b: ");scanf("%d",&b);
 	printf("Boi chung nho nhat cua %d va %d la %d",a,b,lcm(a,b));
@@ -12,7 +12,7 @@ int main(){
 
 int lcm(int a, int b){
 	int i,result=0;
-	for (i=1;i<=9999;i++){
+	for (i=1;i<=99999;i++){
 		if (i%a==0 && i%b==0){
 			result=i;
 			break;

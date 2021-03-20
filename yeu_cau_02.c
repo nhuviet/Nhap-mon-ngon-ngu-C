4c3d1f2ffdd38507357d4d5a33c2089acebcb1f0
Viet_Faia <72543029+nhuviet@users.noreply.github.com>
2021-03-20 14:29:08 +0700

    Update yeu_cau_02.c

--- yeu_cau_02.c
+++ yeu_cau_02.c
@@ -4,12 +4,13 @@ float chu_vi(float r);
 float dien_tich(float r);
 float const pi=3.14;
 
-void main(){
+int main(){
 	float r;
 	printf("Tinh chu vi, dien tich hinh tron\n");
 	printf("Nhap ban kinh hinh tron r = ");scanf("%f",&r);
 	printf("Chu vi hinh tron ban kinh %.2f C = %.2f\n",r,chu_vi(r));
 	printf("Dien tich hinh tron ban kinh %.2f S = %.2f",r,dien_tich(r));
+	return 0;
 }
 
 float chu_vi(float r){
